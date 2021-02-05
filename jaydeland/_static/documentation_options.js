0b21c89c3c84f14a6f6faf1942b17d172b3c4bf2
jaydeland <jaydeland@users.noreply.github.com>
2021-02-05 18:33:21 +0000

    deploy: 616f079c5639953a55ca250763460a2c0d892226

--- jaydeland/_static/documentation_options.js
+++ jaydeland/_static/documentation_options.js
@@ -1,12 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.30',
+    VERSION: '5.31',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
 };
